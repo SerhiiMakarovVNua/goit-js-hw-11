140d1f7b44c38a17fd9ac65efca7c7885c9d17f0
SerhiiMakarovVNua <serhiimakarov79@gmail.com>
2024-08-09 16:15:40 +0300

    js-hw-11.02

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,6 @@ const API_KEY = '45301554-42ab9bd7e2b914874aa400849';
 
 export function searchImages(searchField) {
   const url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+encodeURIComponent(searchField)+'&image_type=photo&orientation=horizontal&safesearch=true';
-
   return fetch(url)
     .then(response => {
       if (!response.ok) {
